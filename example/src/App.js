fb8e2b46970549411d30de372f0cc2ab8e77db2d
PasinduS96 <pasindubahagya@gmail.com>
2021-03-12 13:50:43 +0530

    Dynamic options added

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-import { ExampleComponent } from 'dynamic-dropdown'
+import { DropDown } from 'dynamic-dropdown'
 import 'dynamic-dropdown/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const options = [
+    { value: "Spring", label: "Spring", color: "#498205" },
+    { value: "Summer", label: "Summer", color: "#c19c00" },
+    { value: "Autumn", label: "Autumn", color: "#da3b01" },
+    { value: "Winter", label: "Winter", color: "#004e8c" }
+  ];
+
+  const styles = {
+    selection: {width: 300 , borderRadius: 10}
+  }
+
+  return <DropDown elements={options} selection={styles.selection}/>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const DropDown = ({ selection, options, div, elements }) => {
+  return( 
+  <div style={div}>
+  <select style={selection}>
+    <option style={options} value="0">Select car:</option>
+    {elements.map(item => {
+      return  <option style={options} value={item.value}>{item.label}</option>
+    })}
+  </select>
+</div>)
+
+
 }

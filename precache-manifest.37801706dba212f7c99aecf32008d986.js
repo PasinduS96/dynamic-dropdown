bc9931032b4b8e3809f4b4ad4ccd33ccb9034514
PasinduS96 <pasindubahagya@gmail.com>
2021-03-12 14:23:22 +0530

    Updates

---
+++ precache-manifest.37801706dba212f7c99aecf32008d986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ed066ba383abe2217b7eac5d9131b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8db82252ac061a2fd9d",
+    "url": "./static/css/main.db990518.chunk.css"
+  },
+  {
+    "revision": "a601afa30ecce0650254",
+    "url": "./static/js/2.b23e6c39.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b23e6c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8db82252ac061a2fd9d",
+    "url": "./static/js/main.cb9908f1.chunk.js"
+  },
+  {
+    "revision": "9e88f2f66969df0ab1f4",
+    "url": "./static/js/runtime-main.09001509.js"
+  }
+]);
